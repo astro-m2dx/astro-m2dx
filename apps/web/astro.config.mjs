08f6317db9e1956ef361f7650988693cc08468d1
Christian Fuss <christian@hackyourshack.org>
2022-09-08 09:35:54 +0200

    Setup initial structure of web site
    - tailwind
    - page layout
    - etc...

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import autoLayout from '@astro-m2dx/remark-astro-auto-layout';
 import mdx from '@astrojs/mdx';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
-    markdown: {
-        remarkPlugins: [autoLayout],
-        extendDefaultPlugins: true,
-    },
+  integrations: [mdx(), tailwind()],
+  markdown: {
+    remarkPlugins: [autoLayout],
+    extendDefaultPlugins: true
+  }
 });
---
+++ apps/web/src/client.ts
@@ -0,0 +1,4 @@
+console.log('Hello from Astro-M2DX');
+
+// Required to make this a module
+export {};
